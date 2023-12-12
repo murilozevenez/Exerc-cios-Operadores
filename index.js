3c9ec90b2f744f7c4f257642a571f8135acdcae5
murilo zevenez <murilozevenez89@gmail.com>
2023-12-12 19:59:26 -0300

    Exercícios Operadores

---
+++ index.js
@@ -0,0 +1,35 @@
+//
+salario1 = 100
+salario2 = 200
+salario3 = 300
+
+ir1 = 0
+ir2 = 10
+ir3 = 15
+
+salliq1 = 100
+salliq2 = 190
+salliq3 = 285
+
+console.log (salliq1 >= salario1)
+console.log (salliq2 < salario2)
+console.log (salliq3 == (salario3 - ir3))
+
+A = 3
+B = 7
+C = 4
+
+console.log((A+C) > B )
+console.log(B>=(A+2))
+console.log(C == (B-A))
+console.log((B+A) <= C)
+console.log((C+A) >B)//
+
+A=5
+B=4
+C=3
+D=6
+
+console.log((A>C)&&(C<=D))
+console.log((A+B)>10||(A+B)==(C+D))
+console.log((A>=C)&&(D>=C))
